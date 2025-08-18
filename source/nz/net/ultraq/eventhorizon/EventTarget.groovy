ed52e8980c29211991b8e0116e7a568743285aaf
Emanuel Rabina <emanuelrabina@gmail.com>
2025-08-18 17:21:30 +1200

    Remove unused import

--- source/nz/net/ultraq/eventhorizon/EventTarget.groovy
+++ source/nz/net/ultraq/eventhorizon/EventTarget.groovy
@@ -19,7 +19,6 @@ package nz.net.ultraq.eventhorizon
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import groovy.transform.PackageScope
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.ExecutorService
